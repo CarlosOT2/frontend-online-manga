0849ed05e4c021fdb39bb99eab4412f784813207
CarlosOT <135380086+CarlosOT2@users.noreply.github.com>
2026-07-23 11:56:05 -0300

    Minor Change formatDate

--- src/Shared/utils/formatDate.ts
+++ src/Shared/utils/formatDate.ts
@@ -9,9 +9,9 @@ export default function formatDate(dateStr: any) {
     const [year, month, day] = dateStr.split('-').map(Number)
     const date = new Date(year, month - 1, day)
 
-    return date.toLocaleDateString(navigator.language, {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit'
+    return date.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
     })
 }
